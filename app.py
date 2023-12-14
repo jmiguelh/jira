a33435a8a006e6d3ec5b98b140c1e9f3e82ed9c0
Miguel Henriques <github@jmh.com.br>
2023-12-14 08:05:58 -0300

    Primeira página

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 import time
 import jira
+import models.painel as painel
 
 TIPO = ["Todos", "Evolutivo", "Corretivo"]
 SETOR = ["Todos", "CRL", "Comercial", "Têxtil"]
@@ -15,6 +17,13 @@ STATUS = [
     "Systextil",
 ]
 
+st.set_page_config(
+    page_title="Lunelli - Salesforce Squad",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
 with st.sidebar:
     st.image("img/lunelli.png", width=250)
     with st.expander(":pushpin: Filtos"):
@@ -27,18 +36,42 @@ with st.sidebar:
         if st.button("Recarregar dados"):
             with st.spinner("Carregando..."):
                 jira.carregar()
-            st.success("Sucesso!")
+            st.success("Sucesso! Repocessando...")
             time.sleep(3)
             st.rerun()
 
     st.write("")
     st.image("img/salesforce_logo.png", width=150)
 
-
 topo = st.container()
 topo.title("Salesforce Squad")
 
-print(status)
+corpo = st.container()
+a, b, c = corpo.columns(3)
 
-# pd.DataFrame()
-# conn = st.connection()
+a.metric(
+    label="Total de cards abertos",
+    value=painel.total_cards(),
+    delta=painel.cards_aberto_ultimo_dia(),
+)
+a.metric(
+    label="Cards abertos no Mês",
+    value=painel.cards_aberto_no_mes(),
+)
+
+b.write("Cards aberto por mês")
+b.bar_chart(painel.cards_por_mes())
+
+fig = px.pie(
+    painel.cards_por_setor(), values="Quantidade", names="Setor", width=450, height=450
+)
+
+c.write("Cards aberto por setor")
+c.plotly_chart(fig)
+
+# c.bar_chart(painel.cards_por_setor())
+
+rodape = st.container()
+a, b = rodape.columns(2)
+
+a.bar_chart(painel.cards_por_setor_status())
---
+++ models/painel.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+from pony.orm import *
+import pandas as pd
+
+
+db = Database()
+
+
+@db_session
+def total_cards() -> int:
+    sql = "select count(1) from jira_card where status_agrupado <> 'Cancelado'"
+    result = db.select(sql)
+    return result[0]
+
+
+@db_session
+def cards_aberto_ultimo_dia() -> int:
+    sql = "select count(1) from jira_card where status_agrupado <> 'Cancelado' and criado > date('now','-1 day')"
+    result = db.select(sql)
+    return result[0]
+
+
+@db_session
+def cards_aberto_no_mes() -> int:
+    sql = "select count(1) from jira_card where status_agrupado <> 'Cancelado' and criado > date('now','start of month')"
+    result = db.select(sql)
+    return result[0]
+
+
+@db_session
+def cards_por_mes():
+    sql = """SELECT strftime('%Y-%m',criado), 
+            sum(CASE
+                WHEN tipo_agrupado = "Evolutivo" THEN 1
+                ELSE 0
+            END) AS Evolutivo,
+            sum(CASE
+                WHEN tipo_agrupado = "Corretivo" THEN 1
+                ELSE 0
+            END) AS Corretivo
+            FROM jira_card
+            WHERE status_agrupado <> 'Cancelado'
+            GROUP BY strftime('%Y-%m',criado)
+            ORDER BY 1 DESC
+            LIMIT 6"""
+    result = db.select(sql)
+    df = pd.DataFrame(
+        result,
+        columns=["Mês", "Evolutivo", "Corretivo"],
+    )
+    df = df.set_index("Mês")
+    return df
+
+
+@db_session
+def cards_por_setor():
+    sql = """SELECT pai, count(1)
+            FROM jira_card
+            WHERE status_agrupado <> 'Cancelado'
+            GROUP BY pai"""
+    result = db.select(sql)
+    df = pd.DataFrame(
+        result,
+        columns=["Setor", "Quantidade"],
+    )
+    # df = df.set_index("Setor")
+    return df
+
+
+@db_session
+def cards_por_setor_status():
+    sql = """SELECT pai, 
+            sum(CASE
+                WHEN tipo_agrupado = "Evolutivo" THEN 1
+                ELSE 0
+            END) AS Evolutivo,
+            sum(CASE
+                WHEN tipo_agrupado = "Corretivo" THEN 1
+                ELSE 0
+            END) AS Corretivo
+            FROM jira_card
+            WHERE status_agrupado <> 'Cancelado'
+            GROUP BY pai"""
+    result = db.select(sql)
+    df = pd.DataFrame(
+        result,
+        columns=["Setor", "Evolutivo", "Corretivo"],
+    )
+    df = df.set_index("Setor")
+    return df
+
+
+db.bind(provider="sqlite", filename="../data/db.sqlite", create_db=True)
+
+db.generate_mapping(create_tables=True)
